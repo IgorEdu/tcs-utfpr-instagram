36e6171a28737b47e9d7d4be5e83a9fa9a782185
Igor <igoreduardo20101@gmail.com>
2026-04-25 21:21:29 -0300

    refact: limpa arquivos de setup do Vue

---
+++ frontend-utfpr-instagram/src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <main class="login-container">
+    <h1>Entrar no Instagram</h1>
+    <p>A interface de Login será desenvolvida aqui.</p>
+  </main>
+</template>
+
+<style scoped>
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
+</style>
